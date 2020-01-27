7b9489e84a37a2eb8f555a97d71d8d7976cd98d0
Tony <60369491+ExcellentMacaroon@users.noreply.github.com>
2020-01-27 15:50:14 -0800

    Added via upload
    .exe .cpp and .dev files for my hello-world program.
    
    coded in: GNU C++ 11 using: Dev-C++ version 5.11

---
+++ main.cpp
@@ -0,0 +1,13 @@
+/* This is the program I will use to figure out how GitHub works, I will also use the hello-world directory to work with and learn C++ */
+
+#include <iostream> // This allows us to print things onto the console
+
+using namespace std; // This line allows us to abandon the std:: part of the cout and cin in our program
+
+
+int main() 
+{
+	cout << "Hello, world!\n"; //This is all the program requires to complete.
+	
+	return 0;
+}
